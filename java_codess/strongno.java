9cea807d04818f3712d1d232819f07ab45eca3da
chandu <hemachandubenuboyina@gmail.com>
2025-08-08 23:46:29 +0530

    basic searching techniques

---
+++ java_codess/strongno.java
@@ -0,0 +1,5 @@
+package java_codess;
+
+public class strongno {
+    
+}
--- positive_or_negative.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter an intger"))
-if n>=0:
-    if n==0:
-        print("number is zero")
-    else:
-        print("postitive number")
-else:
-    print("negative number")
