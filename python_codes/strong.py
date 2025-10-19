2738dd31e10de2b47543c79fcb8a61fa17caf3c1
chandu <hemachandubenuboyina@gmail.com>
2025-10-19 22:35:59 +0530

    updated codes

---
+++ python_codes/strong.py
@@ -0,0 +1,16 @@
+def strongno(n):
+    k=n
+    s=0
+    while n!=0:
+        f=1
+        r=n%10
+        for i in range(1,r+1):
+            f=f*i
+        s+=f
+    if s==k:
+        return f"{k} is strong no"
+    else:
+        return f"{k} is not strong no"
+        
+n=int(input())
+print(strongno(n))
