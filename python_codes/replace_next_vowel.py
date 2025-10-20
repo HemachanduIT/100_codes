b96493d46de5f618cf251783b04a9e76e56c8b48
chandu <hemachandubenuboyina@gmail.com>
2025-10-20 23:05:35 +0530

    updated

--- python_codes/replace_next_vowel.py
+++ python_codes/replace_next_vowel.py
@@ -4,7 +4,7 @@ res=''
 for i in range(len(s)):
     if s[i] in v:
         for j in range(len(s)-1):
-            if s[i]==s[j]:
+            if s[i]==v[j]:
                 res+=v[j+1]
     else:
         res+=s[i]
