1f60284d35cbdd0894e347d49d89cc1f3a3e7eba
chandu <hemachandubenuboyina@gmail.com>
2025-10-24 23:52:22 +0530

    updated codes

---
+++ python_codes/revision.py
@@ -0,0 +1,41 @@
+#factorial
+# n=int(input())
+# f=1
+# for i in range(1,n+1):
+#     f=f*i
+# print(f)
+
+
+#palindrome n0
+# n=int(input())
+# r=0
+# k=n
+# while n!=0:
+#     r=r*10+n%10
+#     n//=10
+# if k==r:
+#     print("palindrome")
+# else:
+#     print("not")
+
+#palindrome string
+# s=input()
+# r=''
+# for i in s:
+#     r=i+r
+# if r==s:
+#     print('yes')
+# else:
+#     print('not')
+# # if s==s[::-1]:
+#     print('palindrome')
+# else:
+#     print('not')
+
+#swaping
+# a,b=10,20
+# print('before ',a,b)
+# a,b=b,a
+# print('after',a,b)
+
+#prime no
